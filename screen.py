59ce21fe5292675738dcd31d6e7cf300f704ee85
Rob Chiocchio <RobethX@users.noreply.github.com>
2018-11-09 19:08:40 -0500

    put IP in str()

--- screen.py
+++ screen.py
@@ -14,7 +14,7 @@ import subprocess
 def getIP(): #should I use psutil instead of socket?
     cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE).communicate()[0] #TODO can I do this a better way????
-    return "IP: " + IP #socket.gethostbyname(socket.gethostname())
+    return "IP: " + str(IP) #socket.gethostbyname(socket.gethostname())
 
 def getStatus():
     return "Status: " + str(True) #TODO #str(r.isRunning)
